20b572a64c91880d8cb3b32f44c3d5e5cf94face
max <meison98@bk.ru>
2019-04-16 18:27:30 +0300

    Все оформил, добавил файлы для тестирования

--- views/site/index.php
+++ views/site/index.php
@@ -8,6 +8,10 @@ $this->title = 'My Yii Application';
 <div class="site-index">
     <?=FileRead::widget(['path' => Yii::getAlias('@webroot').'/test.txt', 'searchStr' => 'venenatis' ]); ?>
     <hr/>
+    <?=FileRead::widget(['path' => Yii::getAlias('@webroot').'/test.txt', 'searchStr' => 'venenatis', 'mimeType'=> 'text/plain' ]); ?>
+    <hr/>
+    <?=FileRead::widget(['path' => Yii::getAlias('@webroot').'/test.txt', 'searchStr' => 'venenatis', 'mimeType'=> 'image/plain' ]); ?>
+    <hr/>
     <?=FileRead::widget(['path' => Yii::getAlias('@webroot').'/test.txt', 'searchStr' => 'venenatis' , 'maxSize' => '1KB' ]); ?>
     <hr/>
     <?=FileRead::widget(['path' => Yii::getAlias('@webroot').'/test.txt', 'searchStr' => 'venenatis' , 'maxSize' => '100KB' ]); ?>
@@ -18,7 +22,7 @@ $this->title = 'My Yii Application';
     <hr/>
     <?=FileRead::widget(['path' => 'https://www.w3.org/TR/PNG/iso_8859-1.txt', 'searchStr' => 'BRssOKEN' ]); ?>
     <hr/>
-    <?=FileRead::widget(['path' => 'https://www.w3.org/TR/PNG/iso_885232332239-1', 'searchStr' => 'BRssOKEN' ]); ?>
+    <?=FileRead::widget(['path' => 'https://tehnot.com/wp-content/uploads/2017/03/android-o-00.jpg', 'searchStr' => 'BRssOKEN' ]); ?>
     <hr/>
 
 </div>
